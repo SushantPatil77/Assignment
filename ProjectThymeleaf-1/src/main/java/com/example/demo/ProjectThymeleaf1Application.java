85efadef0cf4217e70e4f17f5a18b167f7f047ec
SushantPatil77 <91300978+SushantPatil77@users.noreply.github.com>
2022-02-21 13:06:55 +0530

    Add files via upload

---
+++ ProjectThymeleaf-1/src/main/java/com/example/demo/ProjectThymeleaf1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectThymeleaf1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectThymeleaf1Application.class, args);
+	}
+
+}
---
+++ ProjectThymeleaf-1/src/main/java/com/example/demo/Repo/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.Repo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.enitiy.Student;
+
+
+
+
+
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+}
